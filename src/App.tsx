1371c84b41a21b44cf2a52b9aba850735aa77be0
CesarBego <cesar@bego.ai>
2025-04-16 16:59:02 -0600

    import context tools and change states

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import VehicleForm from "./components/VehicleForm";
+import { useContext } from "react";
+import { AppContext } from "./context/App.context";
+import { Box, Paper } from "@mui/material";
+import ResponseIMage from "./components/ResponseIMage";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { step, setStep, setImageUrl } = useContext(AppContext);
+
+  const goBack = () => {
+    setStep(0);
+    setImageUrl("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box
+        sx={{
+          width: "100%",
+        }}>
+        {step === 0 && <VehicleForm />}
+        {step === 1 && <ResponseIMage onClick={goBack} />}
+      </Box>
+
+      <p className="read-the-docs">Vehicle Publisher</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
